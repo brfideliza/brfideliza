c5304b32fa037f7bd8f6d042be7c5ecc160b6d94
Jonatas <jonatasmateuscontato@gmail>
2023-07-13 16:59:42 -0300

    Update Firebase options

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:login_flutter/firebase_options.dart';
 import 'package:login_flutter/screens/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MyApp()); 
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp (
-      title: 'Email And Password Login',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const LoginScreen()
-    );
+    return MaterialApp(
+        title: 'Email And Password Login',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: const LoginScreen());
   }
 }
