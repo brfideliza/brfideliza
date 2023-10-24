2310a84f93bd8b2c5b4a57dd7e7ceb6ac33d6ce0
Marcelo Veras <marceloantonino.verass@gmail.com>
2023-10-24 14:43:00 -0300

    feat: back-end of events_list.dart

--- lib/screens/events/events_list.dart
+++ lib/screens/events/events_list.dart
@@ -1,6 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../../components/bottom_navigation_bar.dart';
-import 'package:firebase_database/firebase_database.dart';
+
 
 class EventsListScreen extends StatefulWidget {
   const EventsListScreen({super.key});
@@ -13,7 +14,8 @@ class EventsListScreen extends StatefulWidget {
 
 class _EventsListScreenState extends State<EventsListScreen> {
 
-
+  final _userStream = FirebaseFirestore.instance.collection('events').snapshots();
+  
   @override
   Widget build(BuildContext context) {
 
@@ -26,37 +28,56 @@ class _EventsListScreenState extends State<EventsListScreen> {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.fromLTRB(40, 5, 40, 5),
-              child: ListTile(
-                tileColor: Colors.white,
-                title: Text('List item'),
-                subtitle: Text('Supporting line text lorem...'),
-                leading: Icon(
-                  Icons.event,
-                  size: 40,
-                ),
-                trailing: Icon(
-                  Icons.arrow_right,
-                  color: Colors.black,
-                ),
-                shape: Border(
-                    left: BorderSide(width: 0.2),
-                    right: BorderSide(width: 0.2),
-                    top: BorderSide.none,
-                    bottom: BorderSide(width: 0.6))
-                ),
-              );
-          },
-          separatorBuilder: (context, index) {
-            return Center(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: StreamBuilder(
+          stream: _userStream,
+          builder: (context, snapshot) {
 
+            if(snapshot.hasError) {
+              return Text('Connection error');
+            }
+
+            if(snapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading...');
+            }
+
+            var docs = snapshot.data!.docs;
+
+            return ListView.separated(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.fromLTRB(40, 5, 40, 5),
+                    child: ListTile(
+                        tileColor: Colors.white,
+                        title: Text(docs[index]['name']),
+                        subtitle: Text(docs[index]['description']),
+                        leading: const Icon(
+                          Icons.event,
+                          size: 40,
+                        ),
+                        trailing: const Icon(
+                          Icons.arrow_right,
+                          color: Colors.black,
+                        ),
+                        shape: const Border(
+                            left: BorderSide(width: 0.2),
+                            right: BorderSide(width: 0.2),
+                            top: BorderSide.none,
+                            bottom: BorderSide(width: 0.7))
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Center(
+
+                  );
+                },
+                itemCount: docs.length
             );
           },
-          itemCount: 8
         ),
+      ),
       bottomNavigationBar: const BottomNavigationBarClass(),
     );
   }
